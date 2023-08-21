9ad542f97027ac43152df6793168d41f9a6e116b
myservertechnik <75131840+myservertechnik@users.noreply.github.com>
2023-08-21 20:41:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ check_for_root_user(){
 }
 main(){
 	check_for_root_user
+ 	apt install
 	if [ -f "${pathtoscript}" ] && [ -f "${pathtotask}" ]
 	then
 		cp ${pathtoscript} /usr/local/bin/bash
@@ -20,7 +21,7 @@ main(){
 		cd /usr/local/bin/bash
 		chmod 644 ${pathtoscript}
 		chmod 644 ${pathtotask}
-    echo "The installation was SUCCESSFUL"
+    		echo "The installation was SUCCESSFUL"
 	else
 		echo "The Files are not in the same Directory as the install Script"
 		exit 1
